5f022e3ded4f09fbcf7512f74278342e9911c0da
martu-giaco <martina.giacomini@davinci.edu.ar>
2025-08-12 09:52:18 -0300

    clase 02

--- act-01/index.js
+++ act-01/index.js
@@ -1,4 +1,23 @@
-const Alumno = require("./alumno.js")
+const Alumno = require("./alumno.js");
+const fs  = require("fs/promises");
+const path = "notas.txt";
+
+const leerArchivo = async () =>{
+    const data = await fs.readFile(path);
+    console.log(data.toString());
+}
+
+const guardar = async (texto) => {
+    await fs.writeFile(path, texto);
+}
+
+// const fn = async() {
+//     await leerArchivo();
+//     await guardar("escribiendo desde node");
+// }
+
+leerArchivo();
+guardar("escribiendo desde node");
 
 let nombre = "mar";
 console.log("hola " + nombre);
