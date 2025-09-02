62e2994bdd99dbf669b6d03239cf91e02c1e5d5c
martu-giaco <martina.giacomini@davinci.edu.ar>
2025-09-02 10:46:02 -0300

    ya no se q poner

--- semana-04/index.js
+++ semana-04/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 //conexiòn con la base de datos
-const urlDB = 'mongodb://localhost:27017';
+const urlDB = process.env.URI_DB;
 mongoose.connect(urlDB);
 const db = mongoose.connection;
 
 db.on('error', () =>{console.error('error de conexión')});
-db.on('opne', () =>{console.error('conoectò la db')});
+db.on('open', () =>{console.error('conoectó la db')});
 
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use('/', (request, response) =>{
